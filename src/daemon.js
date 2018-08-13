192d575a8f62b87a62f58102c78d0151bdc06e52
balazs-kovacs <balazs.kovacs@stylersonline.com>
2018-08-13 15:51:05 +0200

    Refactor daemon

--- src/daemon.js
+++ src/daemon.js
@@ -2,13 +2,12 @@ const Worker = require('./worker');
 
 class Daemon {
   constructor(config) {
-    this.config = config;
+    this.worker = new Worker(config);
   }
 
   run() {
-    const worker = new Worker(this.config);
-    worker.callback = () => worker.run();
-    worker.run();
+    this.worker.callback = () => this.worker.run();
+    this.worker.run();
   }
 }
 
